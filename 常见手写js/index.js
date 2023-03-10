bd8103c2a458b169afd54e745eed7f4d7822ecf4
zhangmingdi <zhangmingdi@shein.com>
2023-03-10 15:17:18 +0800

    手写js

--- 常见手写js/index.js
+++ 常见手写js/index.js
@@ -28,3 +28,62 @@ function deepCopyObject(obj,map = new Map()){
 
   return newObject
 }
+
+// call
+function call(obj,...args){
+  var fn = this
+  //注意obj可能是非对象类型
+  const vo = obj ? Object(obj) : window
+  const key = new Symbol('l')
+  vo[key]=fn
+  vo[key](...args)
+  delete  vo[key]
+}
+
+// apply
+function call(obj,args){
+  var fn = this
+  const vo = obj ? Object(obj) : window
+  const key = new Symbol('l')
+  vo[key]=fn
+  vo[key](...args)
+  delete  vo[key]
+}
+
+// bind 
+function bind (obj,...args){
+  const fn = this
+  return function(...arr){
+    fn.call(obj,...args,...arr)
+  }
+}
+// new 操作
+function newFn(fn,...args){
+  const vo = {}
+ vo.__proto__ = fn.prototype;
+ const res= fn().call(vo,...args)
+ return typeof res==='object'?res:vo
+}
+
+
+// 防抖
+function debounce(fn,timeout){
+  let timer
+  return function(...args){
+    const that = this
+    if(timer) return clearTimeout(timer)
+    timer = setTimeout(()=>{
+      fn.call(that,...args)
+    },timeout)
+  }
+}
+// compose函数
+function compose(...args){
+  return function (){
+    if(args.length===0)return
+    if(args.length===1)return args[0](arguments[0]) 
+    return args.slice(1).reduce((pre,cur)=>{
+      return cur(pre)
+    },args[0](arguments[0]))
+  }
+}
